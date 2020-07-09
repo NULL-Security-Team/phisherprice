8183f2a96a5b05b72dbabbc54f133f8e86a2268e
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2020-07-10 00:42:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ echo -e '\e[1;33m
         \/NULLSecurity Team\/       \/        \/     \/     \/ \e[1;34m
 '
 echo "[✔] Checking directories...";
-if [ -d "/usr/share/doc/phisherprice" ] ;
+if [ -d "/usr/local/bin/phisherprice" ] ;
 then
 echo "[◉] A directory phisherprice was found! Do you want to replace it? [Y/n]:" ; 
 read selection
 if [ $selection == "y" ] ; 
 then
- rm -r "/usr/share/doc/phisherprice"
+ rm -r "/usr/local/bin/phisherprice"
 else
  exit
 fi
@@ -23,11 +23,11 @@ fi
 
 echo "[✔] Installing ...";
 echo "";
-git clone https://github.com/NULL-Security-Team/phisherprice /usr/share/doc/phisherprice;
+git clone https://github.com/NULL-Security-Team/phisherprice /usr/local/bin/phisherprice;
 echo "#!/bin/bash 
-bash /usr/share/doc/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
+bash /usr/local/bin/phisherprice/phisherprice.sh" '${1+"$@"}' > phisherprice;
 echo "#!/bin/bash 
-python /usr/share/doc/phisherprice/sshscan.py" '${1+"$@"}' > sshscan;
+python /usr/local/bin/phisherprice/sshscan.py" '${1+"$@"}' > sshscan;
 chmod +x phisherprice;
 chmod +x sshscan;
 sudo cp phisherprice.sh /usr/bin/;
